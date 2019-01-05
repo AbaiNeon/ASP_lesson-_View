a03ed9701acfd5cd6f2d2aca4278694235403df0
ТогузбаевА <abai2010@bk.ru>
2019-01-05 13:10:05 +0600

    Добавьте файлы проекта.

---
+++ ViewsLesson/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewsLesson.Models
+{
+    public class Player : Entity
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public virtual Team Team { get; set; }
+
+       
+    }
+}
